8aea21f8eb9d9ecc5c92e5a2b65690bb73198ac3
thevamshikrishna <vamshikrishnaiam7@gmail.com>
2025-06-11 15:50:11 +0530

    Update

--- June Month/Arrays and ArrayList/RotateArray.java
+++ June Month/Arrays and ArrayList/RotateArray.java
@@ -1,2 +1,45 @@
 public class RotateArray {
-}
+        // Function to rotate an array by d elements in counter-clockwise direction.
+        static void rotateArr(int arr[], int d) {
+            int n=arr.length;
+            d=d%n;
+
+
+            reverse(arr,0,d-1);
+            reverse(arr,d,n-1);
+            reverse(arr,0,n-1);
+        }
+
+        static void reverse(int arr[],int start,int end){
+            while(start<end){
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    /*
+    Rotate Array
+
+Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+
+Note: Consider the array as circular.
+
+Examples :
+
+Input: arr[] = [1, 2, 3, 4, 5], d = 2
+Output: [3, 4, 5, 1, 2]
+Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
+Input: arr[] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], d = 3
+Output: [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
+Explanation: when rotated by 3 elements, it becomes 8 10 12 14 16 18 20 2 4 6.
+Input: arr[] = [7, 3, 9, 1], d = 9
+Output: [3, 9, 1, 7]
+Explanation: when we rotate 9 times, we'll get 3 9 1 7 as resultant array.
+Constraints:
+1 <= arr.size(), d <= 105
+0 <= arr[i] <= 105
+     */
