63a2f2630814c0f623d17c6fa5ba96009ddc9461
thevamshikrishna <vamshikrishnaiam7@gmail.com>
2025-06-30 10:35:09 +0530

    Update

--- June Month/Projects/StudentMangementSystem/src/main/java/com/atyeti/dbConfig/DbQueries.java
+++
@@ -1,6 +0,0 @@
-package com.atyeti.dbConfig;
-
-public interface DbQueries {
-    String LOGIN = "SELECT * FROM students WHERE id = ? AND password = ?";
-    String REGISTER="INSERT INTO students (name, gmail, password) VALUES (?, ?, ?)";
-}
