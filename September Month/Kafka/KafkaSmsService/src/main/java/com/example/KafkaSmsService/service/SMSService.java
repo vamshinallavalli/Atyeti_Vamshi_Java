71713d38b237e816a30f6ddd5096c97b6afaa19d
vamshinallavalli <vamshi.nallavalli@atyeti.com>
2025-09-15 12:55:50 +0530

    Update

--- September Month/Kafka/KafkaSmsService/src/main/java/com/example/KafkaSmsService/service/SMSService.java
+++ September Month/Kafka/KafkaSmsService/src/main/java/com/example/KafkaSmsService/service/SMSService.java
@@ -1,4 +1,11 @@
+package com.example.KafkaSmsService.service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import javax.annotation.PostConstruct;
 
 @Service
@@ -10,7 +17,8 @@ public class SMSService {
     @Value("${twilio.auth.token}")
     private String authToken;
 
-    public static final String FROM_NUMBER = "+919676278087";
+    @Value("${twilio.from.number}")
+    public String FROM_NUMBER;
 
     @PostConstruct
     public void initTwilio() {
@@ -18,7 +26,7 @@ public class SMSService {
     }
 
     public void sendSMS(String toNumber, String textMessage) {
-        Message message = Message.creator(new PhoneNumber(toNumber), new PhoneNumber(FROM_NUMBER), textMessage).create();
+        Message message = Message.creator(new com.twilio.type.PhoneNumber(toNumber), new PhoneNumber(FROM_NUMBER), textMessage).create();
         System.out.println("SMS sent successfully with SID: " + message.getSid());
     }
 }
