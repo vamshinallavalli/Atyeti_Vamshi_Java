d6a5ad0c5773d02bbb03e9bce52bc2985f57d222
vamshinallavalli <vamshi.nallavalli@atyeti.com>
2025-09-15 11:32:25 +0530

    Update

--- September Month/Kafka/KafkaSmsService/src/main/java/com/example/KafkaSmsService/service/SMSService.java
+++ September Month/Kafka/KafkaSmsService/src/main/java/com/example/KafkaSmsService/service/SMSService.java
@@ -1,22 +1,27 @@
-package com.example.KafkaSmsService.service;
-
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import javax.annotation.PostConstruct;
 
 @Service
 public class SMSService {
-    public static final String ACCOUNT_SID="ACbc1f7a3898128c481c3196d8c90f228f";
-    public static final String AUTH_TOKEN="42be0d0279dc6caddd15b8c8a90c6d91";
-    public static final String FROM_NUMBER="+919676278087";
 
+    @Value("${twilio.account.sid}")
+    private String accountSid;
 
-    public void sendSMS(String toNumber,String textMessage){
-        Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
-        Message message=Message.creator(new PhoneNumber(toNumber),new PhoneNumber(FROM_NUMBER),
-                textMessage).create();
+    @Value("${twilio.auth.token}")
+    private String authToken;
 
-        System.out.println("SMS sent  successfully with SID! ");
+    public static final String FROM_NUMBER = "+919676278087";
+
+    @PostConstruct
+    public void initTwilio() {
+        Twilio.init(accountSid, authToken);
+    }
+
+    public void sendSMS(String toNumber, String textMessage) {
+        Message message = Message.creator(new PhoneNumber(toNumber), new PhoneNumber(FROM_NUMBER), textMessage).create();
+        System.out.println("SMS sent successfully with SID: " + message.getSid());
     }
 }
+//    public static final String ACCOUNT_SID="ACbc1f7a3898128c481c3196d8c90f228f";
+//    public static final String AUTH_TOKEN="42be0d0279dc6caddd15b8c8a90c6d91";
+//    public static final String FROM_NUMBER="+919676278087";
