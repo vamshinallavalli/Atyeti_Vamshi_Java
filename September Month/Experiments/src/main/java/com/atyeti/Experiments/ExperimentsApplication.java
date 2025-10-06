580efe01ac8587e079b9651bf6a0dfa0435c1007
vamshinallavalli <vamshi.nallavalli@atyeti.com>
2025-10-06 10:18:04 +0530

    Update

---
+++ September Month/Experiments/src/main/java/com/atyeti/Experiments/ExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.atyeti.Experiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentsApplication.class, args);
+    }
+
+}
